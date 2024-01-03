75b12ccc798f5c6f87caa197c0310447f43eeb3a
rkdtnlzl <rkdtnlzl@naver.com>
2024-01-03 15:05:13 +0900

    [Feat] NoStoryboard Setting

---
+++ MovieTrending/MainViewController/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  MovieTrending
+//
+//  Created by 강석호 on 2024/01/03.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "Main View"
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
